55e011e0c99296b398624d0bcbc31a6fe2e0971d
Maisy Rahmawati <maisy.rahmawati@gmail.com>
2020-10-09 19:09:08 +0700

    feat: tutorial 3 - Modified code and Finished README.MD (result of clone)

---
+++ haidokter/src/main/java/apap/tutorial/haidokter/service/ObatService.java
@@ -0,0 +1,8 @@
+package apap.tutorial.haidokter.service;
+
+import apap.tutorial.haidokter.model.ObatModel;
+import java.util.List;
+
+public interface ObatService {
+    void addObat(ObatModel obat);
+}
--- haidokter/src/main/java/apap/tutorial/haidokter/service/ResepService.java
+++ haidokter/src/main/java/apap/tutorial/haidokter/service/ResepService.java
@@ -7,11 +7,16 @@ public interface ResepService {
     // untuk menambahkan Resep
     void addResep(ResepModel resep);
 
-    void deleteResep(ResepModel resep);
+    // void deleteResep(ResepModel resep);
 
     // untuk mendapatkan semua data Resep yang telah terseimpan
     List<ResepModel> getResepList();
 
     // untuk mendapatkan data sebuah resep berdasarkan nomor resep
-    ResepModel getResepByNomorResep(String noResep);
+    ResepModel getResepByNomorResep(Long noResep);
+
+    // untuk update resep
+    ResepModel updateResep(ResepModel resepModel);
+
+    List<ResepModel> getAllResepDesc();
 }
