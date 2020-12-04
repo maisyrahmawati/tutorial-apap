b341bb574baa6c98ea18ad41d175e32e5af4dd6b
unknown <maisy.rahmawati@gmail.com>
2020-12-04 17:19:48 +0700

    feat: tutorial 7 - make change App.js and answer for latihan nomor 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,57 +5,58 @@ import listMovies from "movies.json";
 import './App.css';
 
 export default class App extends React.Component {
-  state = {
-    favItems: [],
-};
+    state = {
+      favItems: [],
+    };
 
-handleItemClick = (item) => {
-    const newItems = [ ...this.state.favItems];
-    const newItem = { ...item };
-    const targetInd = newItems.findIndex((it) => it.id === newItem.id);
-    if (targetInd < 0) newItems.push(newItem);
-    else newItems.splice(targetInd, 1);
-    this.setState({ favItems: newItems });
-};
+    handleItemClick = (item) => {
+        const newItems = [ ...this.state.favItems];
+        const newItem = { ...item };
+        const targetInd = newItems.findIndex((it) => it.id === newItem.id);
+        if (targetInd < 0) newItems.push(newItem);
+        else newItems.splice(targetInd, 1);
+        this.setState({ favItems: newItems });
+    };
 
-deleteList = (listMovies) {
-  const {list} = this.state;
-  if (listMovies > 0) this.setState({ list: list.filter(list => list != listMovies)})
-}
+    deleteList (listMovies) {
+        const list = [ ...this.state.favItems];
+        if (listMovies > 0) this.setState({ list: list.filter(list => list != listMovies)});
+    };
 
-render() {
-    const { favItems } = this.state;
+    render() {
+        const { favItems } = this.state;
 
-    return (
-        <div className="container-fluid">
-            <h1 className="text-center mt-3 mb-0">Favorites Movie App</h1>
-            <p className="text-center text-secondary text-sm font-italic">
-                (This is a <strong>class-based</strong> application)
-            </p>
-            <div className="container pt-3">
-                <div className="row">
-                    <div className="col-sm">
-                        <List
-                            title="List Movies"
-                            items={listMovies}
-                            onItemClick={() => {}}
-                        />
-                    </div>
-                    <div className="col-sm">
-                        <button
-                          type="button"
-                          className="list-group-item list-group-item-action flex-column align-items-start w-100 mb-3"
-                          onClick={this.deleteList}
-                        >
-                        <List
-                            title="My Favorites"
-                            items={favItems}
-                            onItemClick={this.handleItemClick}
-                        />
+        return (
+            <div className="container-fluid">
+                <h1 className="text-center mt-3 mb-0">Favorites Movie App</h1>
+                <p className="text-center text-secondary text-sm font-italic">
+                    (This is a <strong>class-based</strong> application)
+                </p>
+                <div className="container pt-3">
+                    <div className="row">
+                        <div className="col-sm">
+                            <List
+                                title="List Movies"
+                                items={listMovies}
+                                onItemClick={() => {}}
+                            />
+                        </div>
+                        <div className="col-sm">
+                            <List
+                                type="List"
+                                title="My Favorites"
+                                items={favItems}
+                                onItemClick={this.handleItemClick}
+                            />
+                            <button
+                              type="button"
+                              className="list-group-item list-group-item-action flex-column align-items-start w-100 mb-3"
+                              onClick={this.deleteList("List")}
+                            >Delete</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
